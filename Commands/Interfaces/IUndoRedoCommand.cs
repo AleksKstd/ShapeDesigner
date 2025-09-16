a787573384919a34b021ef557cc7bf66834dc107
AleksKstd <akostadinov2004@gmail.com>
2025-09-17 00:15:01 +0300

    Adding the logic files

---
+++ Commands/Interfaces/IUndoRedoCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeDesigner.Commands.Interfaces
+{
+    public interface IUndoRedoCommand
+    {
+        void Undo();
+        void Redo();
+    }
+}
